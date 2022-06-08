ac5233f7f5d8ba802ad188ee6a91a79c15e8ba0b
royal-dargon <2312749625@qq.com>
2022-06-08 09:24:52 +0800

    add:add some codes

--- backend/app/api/user.py
+++ backend/app/api/user.py
@@ -2,7 +2,7 @@ from . import api
 from .. import cur, conn
 from flask import jsonify, request
 from psycopg2 import extras as ex
-from ..model import UserInfo,ManagerInfo
+from ..model import UserInfo,ManagerInfo, LogInfo
 
 
 # 这是注册用户的接口
@@ -71,7 +71,6 @@ def login():
     }), 400
 
 
-
 # 这是管理员登录的接口
 @api.route("/mlogin", methods=["POST"])
 def manager_login():
@@ -81,7 +80,7 @@ def manager_login():
     if manager.managername is None or manager.pwd is None:
         return jsonify({
             "msg": "信息不能填写为空"
-        }),400
+        }), 400
     sql = "select pwd from manager where mname = %s;"
     cur.execute(r"select id, pwd from manager where mname = '%s';" % manager.managername)
     rows = cur.fetchall()
@@ -142,15 +141,8 @@ def manager_register():
 # 这是展示的主界面，登录进去后，首先展示的便是所有人发布的博客信息
 @api.route("/home/log", methods=["GET"])
 def home_info():
-    # sql = "Select * from log_info"
     cur.execute("Select * from log_info;")  # 对博客表做查询操作,将所有人的博客信息提取出来
     rows = cur.fetchall()
-
-    # if rows is None:  # 表示当前没有博客信息
-    #     return jsonify({
-    #         "masg": "当前没有博客信息"
-    #     }), 400
-
     list1 = []
     for row in rows:  # 把提取的博客信息展示
         content = {'log_id': row[0], 'model_id': row[1], 'user_id': row[2], 'title': row[3], 'create_time': row[5]}
@@ -162,19 +154,80 @@ def home_info():
 # 这里是通过点击id对博客的详细内容进行查看，同时也是展示了其相关的评论
 @api.route("/home/log/<int:id>", methods=["GET"])
 def log_info(id):
-    pass
+    # 先从前端获取博客id号
+    log_id = id
+    cur.execute(r"select * from log_info where log_id = %s;" % log_id)
+    rows = cur.fetchall()
+    user_id = rows[0][2]
+    cur.execute(r"select uname from user_info where id = %s;" % user_id)
+    rows3 = cur.fetchall()
+    user_name = rows3[0][0]
+    cur.execute(r"select * from comment_info where log_id = %s;" % log_id)
+    rows2 = cur.fetchall()
+    list1 = []
+    # if rows2 is None:
+    #     return jsonify({
+    #         "msg": "暂时没有评论",
+    #         "user_name": user_name,
+    #         "title": rows[0][3],
+    #         "content": rows[0][4],
+    #         "log_time": rows[0][5]
+    #     }), 200
+    for row in rows2:
+        content = {'comment_id': row[0],  'context': row[3], 'time': row[4]}
+        list1.append(content)
+    return jsonify({
+        "msg": "查看成功",
+        "user_name": user_name,
+        "title": rows[0][3],
+        "content": rows[0][4],
+        "log_time": rows[0][5],
+        "comment": list1
+    }), 200
 
 
 # 这里是在博客内容下发布评论
 @api.route("/home/log/<int:id>/comment", methods=["GET"])
 def post_comment():
-    pass
+    # 从前端获取评论内容,博客号,和用户号
+    context = request.form.get("comment")
+    log_id = request.form.get("logid")
+    user_id = request.form.get("userid")
+
+    if context is None:
+        return ({
+            "msg": "未输入评论内容"
+        }), 400
+
+    sql = "Insert into comment_info (log_id,user_id,context) values(%d, %d, %s);"
+    cur.execute(r"Insert into comment_info (log_id,user_id,context) values('%d', '%d', '%s');" % log_id % user_id % context)
+
+    conn.commit()
+
+    return ({
+        "msg": "评论成功"
+    }), 200
 
 
 # 这里是用户发布博客
-@api.route("/home/post_log", methods=["POST"])
-def post_log():
-    pass
+@api.route("/home/post_log/<int:id>", methods=["POST"])
+def post_log(id):
+    blog = LogInfo()
+    blog.user_id = id
+    blog.title = request.form.get('title')
+    blog.content = request.form.get('content')
+    # blog.model_id = request.form.get('model_id')
+    # 现阶段是默认这个数值为1， 后期可以根据选择进行更改
+    blog.model_id = 1
+    if blog.model_id is None or blog.user_id is None or blog.title == '' or blog.content == '':
+        return jsonify({
+            "msg": "有数据为空"
+        }), 400
+    cur.execute(r"insert into log_info (model_id, user_id, title, content) values(%s, %s, '%s', '%s');" % (blog.model_id,blog.user_id, blog.title, blog.content))
+    conn.commit()
+    return jsonify({
+        "msg": "发表成功"
+    }), 200
 
 
 # 这里是查看自己的模板
